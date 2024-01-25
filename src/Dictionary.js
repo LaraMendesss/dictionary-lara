f8a252025456bf3da081c4a5e6c7d5d78dd180d6
LaraMendesss <laraicmendes@gmail.com>
2024-01-25 15:00:50 -0500

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -7,7 +7,6 @@ const [keyword,setKeyword]=useState("");
     function handleResponse(response){
         console.log(response);
     }
-
     function search(event){ 
         event.preventDefault(); 
         let apiKey=`dda9a648t200432eo3334f85db57e348`; 
@@ -15,21 +14,15 @@ const [keyword,setKeyword]=useState("");
 
      axios.get(apiUrl).then(handleResponse);
     } 
-
     function handleChange(event){
         setKeyword(event.target.value);
-   
     }
-
     return ( 
 
         <div className="intro"> 
         <form onSubmit={search}> 
            <input className="" type="search" autoFocus={true} onChange={handleChange}/> 
-
            <input type="submit" value="Search"/> 
-
-
         </form>     
 
             </div>); 
