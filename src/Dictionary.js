25a1cf1f11c078e3a6d2a2c20e6b9c6c0c645ee5
LaraMendesss <laraicmendes@gmail.com>
2024-01-26 13:56:31 -0500

    pictures api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,21 +2,33 @@
  import axios from "axios";
  import Results from "./Results";
  import "./Dictionary.css";
+ import Photos from "./Photos";
 
 export default function Dictionary(props) { 
-const [keyword,setKeyword]=useState(props.keyword);
+const [keyword, setKeyword]=useState(props.keyword);
 const [results, setResults]=useState(null);
-let [loaded, setLoaded] = useState(false);
+const [loaded, setLoaded] = useState(false);
+const[photos, setPhotos]=useState(null);
 
     function handleResponse(response){
 
         setResults(response.data);}
 
+    function handlePhotoResponse(response){
+        setPhotos(response.data.photos);
+    }
+
     function search(){ 
     let apiKey=`dda9a648t200432eo3334f85db57e348`; 
     let apiUrl=`https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
 
-     axios.get(apiUrl).then(handleResponse);} 
+   
+    let photoapiKey=`dda9a648t200432eo3334f85db57e348`;
+    let photoapiUrl=`https://api.shecodes.io/images/v1/search?query=${photos}&key=${photoapiKey}`;
+
+
+     axios.get(photoapiUrl).then(handlePhotoResponse);} 
 
     function handleSubmit(event){
         event.preventDefault(); 
@@ -34,11 +46,12 @@ let [loaded, setLoaded] = useState(false);
     return ( 
 
         <div className="intro"> 
-        <form onSubmit={handleSubmit}> 
+        <form id="form" onSubmit={handleSubmit}> 
            <input className="" type="search" autoFocus={true} placeholder="Type the word here" onChange={handleChange}/> 
            <input type="submit" className="btn btn-primary" value="Search"/> 
         </form>     
         <Results className="results" results={results}/>
+        <Photos photos={photos} />
             </div>);} 
           else{
             load();
---
+++ src/Photos.js
@@ -0,0 +1,20 @@
+import React from "react"; 
+
+export default function Photos(props) {
+  
+    if(props.photos){
+return (
+    <div className="photos">
+        <div className="row">
+    {props.photos.map( function(photo, index)
+    {
+        return(
+        <div className="col-4" key={index}><a href={photo.src.original} target="_blank"rel="noreferrer"><img src={photo.src.landscape} alt="photos" className="img-fluid" key={index}/></a>
+        </div>);
+    })}
+      </div>
+    </div>);
+}
+else{
+    return null;
+}}
