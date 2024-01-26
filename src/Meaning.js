8f99030825b6b0816f456c9c216b43c69caae1a3
LaraMendesss <laraicmendes@gmail.com>
2024-01-25 19:37:28 -0500

    last details

--- src/Meaning.js
+++ src/Meaning.js
@@ -6,7 +6,7 @@ return(
 <div className="Meaning">
 
 <h3 className="text-capitalize name">{props.meaning.partOfSpeech}:</h3> 
-<span className="text-capitalize definition">{props.meaning.definition}.</span>
+<p className="text-capitalize definition">{props.meaning.definition}.</p>
 <p className="text-capitalize example"><strong></strong>{props.meaning.example}</p>
 </div>
 );
