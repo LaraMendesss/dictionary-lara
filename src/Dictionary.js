f5b3e5e06a9522ebbe7bbe8e1cba40847f88ed7e
LaraMendesss <laraicmendes@gmail.com>
2024-01-26 12:09:41 -0500

    css

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,31 +3,46 @@
  import Results from "./Results";
  import "./Dictionary.css";
 
-export default function Dictionary() { 
-const [keyword,setKeyword]=useState("");
+export default function Dictionary(props) { 
+const [keyword,setKeyword]=useState(props.keyword);
 const [results, setResults]=useState(null);
+let [loaded, setLoaded] = useState(false);
 
     function handleResponse(response){
 
-        setResults(response.data);
-        
-    }
-    function search(event){ 
-        event.preventDefault(); 
-        let apiKey=`dda9a648t200432eo3334f85db57e348`; 
-      let apiUrl=`https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+        setResults(response.data);}
 
-     axios.get(apiUrl).then(handleResponse);
-    } 
+    function search(){ 
+    let apiKey=`dda9a648t200432eo3334f85db57e348`; 
+    let apiUrl=`https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+
+     axios.get(apiUrl).then(handleResponse);} 
+
+    function handleSubmit(event){
+        event.preventDefault(); 
+        search();}
+
     function handleChange(event){
-        setKeyword(event.target.value);
-    }
+        setKeyword(event.target.value);}
+
+        function load(){
+            setLoaded(true);
+            search();
+        }
+
+        if(loaded){
     return ( 
 
         <div className="intro"> 
-        <form onSubmit={search}> 
-           <input className="" type="search" autoFocus={true} onChange={handleChange}/> 
+        <form onSubmit={handleSubmit}> 
+           <input className="" type="search" autoFocus={true} placeholder="Type the word here" onChange={handleChange}/> 
            <input type="submit" className="btn btn-primary" value="Search"/> 
         </form>     
         <Results className="results" results={results}/>
             </div>);} 
+          else{
+            load();
+return null;
+
+          }  
+        }
